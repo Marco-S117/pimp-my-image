d6b268957df9491989ab234c49ff2a6be7998ec9
Marco-S117 <polino47@gmail.com>
2020-04-02 17:50:53 +0200

    Finito

--- index.php
+++ index.php
@@ -9,24 +9,104 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <title>Document</title>
 </head>
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="col">
-        <form action="index.php" method="post" enctype="multipart/form-data">
-          Select Image File to Upload:
-          <input type="file" name="file">
-          <select name="filter">
-            <option value="0">Bianco e nero</option>
-            <option value="1">Negativo</option>
-          </select>
-          <input type="submit" name="submit" value="Upload">
-        </form>
-        <?php 
-          if(isset($fileName)) echo('<img src="uploads/' . $fileName . '" class="img-fluid">')
-        ?>
+
+<style>
+</style>
+
+<body class="bg-secondary">
+  <div class="container bg-light">
+
+    <!-- navigation -->
+    <div class="row bg-info">
+      <div class="col-12">
+        <nav class="navbar navbar-expand-lg navbar-light">
+          <a class="navbar-brand text-uppercase" href="#">PmI</a>
+
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Credits</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </li>
+          </ul>
+          
+        </nav>
       </div>
     </div>
+    
+    <!-- main -->
+    <div class="row bg-light">
+      <div class="col-12">
+        <main class="p-4">
+          <h4>Welcome to Pimp my Images</h4>
+          <p>Upload your photos and aplly a filter!</p>
+
+          <div class="my-5 d-flex justify-content-center alert alert-info">
+            <form action="index.php" method="post" enctype="multipart/form-data">
+
+              <label>Select files to upload</label>
+              <div class="input-group mb-3">
+                <div class="custom-file">
+                  <input type="file" name="files[]" class="custom-file-input" id="inputGroupFile03" multiple>
+                  <label class="custom-file-label" for="inputGroupFile03">Choose file</label>
+                </div>
+              </div>
+
+              <label for="filter">Select an effect</label>
+              <div class="input-group mb-3">
+                <select name="filter" class="form-control">
+                  <option value="0">Select a filter</option>
+                  <option value="1">Bianco e nero</option>
+                  <option value="2">Negativo</option>
+                </select>
+              </div>
+
+              <label for="filter"></label>
+              <div class="input-group mb-3">
+                <input type="submit" name="submit" value="Upload" class="form-control">
+              </div>
+
+            </form>
+          </div>
+
+          <!-- show images with watermark and effect -->
+          <div>
+            <?php
+              if(count($warnings) > 0){
+                echo ('<ul class="alert alert-warning px-5">');
+                foreach ($warnings as $warning) {
+                  echo ("<li>File " . $warning . " not allowed</li>");
+                }
+                echo ('</ul>');
+              } 
+            ?>
+          </div>
+          <div>
+            <?php echo $statusMsg ?>
+          </div>
+          <div class="mb-4 p-4 border rounded bg-white">
+            <?php
+              if(count($imgArray) > 0) {
+                echo('<div class="row">');
+                foreach ($imgArray as $img) {
+                  echo('<div class="col-12 col-sm-6 col-md-3 align-self-center"><img src="uploads/' . $img . '" class="img-fluid"></div>');
+                }
+                echo('</div>');
+              }
+            ?>
+          </div>
+        </main>
+      </div>
+    </div>
+
+    <!-- footer -->
+    <div class="row bg-dark text-white text-center text-uppercase">
+      <div class="col-12">
+        <footer class="p-4">Copyright Marco Polino</footer>
+      </div>
+    </div>
+
   </div>
 </body>
 </html>
--- server.php
+++ server.php
@@ -1,53 +1,102 @@
 <?php
 
-  $targetDir = "uploads/"; 
-  $watermarkImagePath = 'watermark.png';
-  $fileName = $_FILES["file"]["name"]; // nome file passato con estensione
+  // check sul submit
+  if( isset($_POST["submit"]) ){
 
+    if($_FILES["files"]['name'] != [""]){
+      
+      $imgArray = [];
+      $warnings = [];
+  
+      // array con i nomi dei file
+      $images = $_FILES['files']['name'];
+  
+      // percorso di destinazione
+      $targetDir = "uploads/";
+      
+      // percorso del watermark
+      $watermarkImagePath = 'watermark.png';
+  
+      // ciclo tutte le immagini
+      foreach ($images as $key => $image) {
+        
+        // prendo il nome del file + estensione
+        $fileName = $image; 
+  
+        // percorso di salvataggio del file
+        $targetFilePath = $targetDir . $fileName; 
+  
+        // prendo formato file
+        $fileType = pathinfo($targetFilePath, PATHINFO_EXTENSION); 
+  
+        // salvo il file caricato nella destinazione $targeFilePath
+        move_uploaded_file($_FILES["files"]["tmp_name"][$key], $targetFilePath);
+  
+        // creo una nuova immagine dal path del watermark
+        $watermarkImg = imagecreatefrompng($watermarkImagePath);
+  
+        // controllo il tipo del file
+        switch($fileType){ 
+          case 'jpg': 
+            $dist = imagecreatefromjpeg($targetFilePath); 
+            // Pusho il nome del file dentro l'array
+            $imgArray[] = $image;
+            break; 
+          case 'jpeg': 
+            $dist = imagecreatefromjpeg($targetFilePath); 
+            // Pusho il nome del file dentro l'array
+            $imgArray[] = $image;
+            break; 
+          case 'png': 
+            $dist = imagecreatefrompng($targetFilePath); 
+            // Pusho il nome del file dentro l'array
+            $imgArray[] = $image;
+            break; 
+          default: 
+            $warnings[] = $images[$key];
+        } 
+        // creo una nuova immagine presa dal percorso di salvataggio
+  
+        // controllo il valore del filtro selezionato
+        if($_POST['filter'] != 0){
+          if($_POST['filter'] == 1) {
+            imagefilter($dist, IMG_FILTER_GRAYSCALE);
+          }
+          if($_POST['filter'] == 2){
+            imagefilter($dist, IMG_FILTER_NEGATE);
+          }
+        }
+        
+        // Setto i margini per il watermark 
+        $marge_right = 50; 
+        $marge_bottom = 50; 
+          
+        // Ottengo altezza/larghezza del watermark
+        $lunghezza = imagesx($watermarkImg); 
+        $altezza = imagesy($watermarkImg); 
+          
+        // Copia l'immagine del watermark sulla nostra foto usando gli offset dei margini e la larghezza della foto per calcolare il posizionamento
+        imagecopy($dist, $watermarkImg, (imagesx($dist) - $lunghezza - $marge_right), (imagesy($dist) - $altezza - $marge_bottom), 0, 0, imagesx($watermarkImg), imagesy($watermarkImg));        
+  
+        // Salvo l'immagine e libero la memoria 
+        imagepng($dist, $targetFilePath); 
+        imagedestroy($dist);
+  
+        $statusMsg = "<h4 class='alert alert-success'>Enjoy your cool photos!</h4>";
+  
+      }
 
-  $targetFilePath = $targetDir . $fileName; // percorso di salvataggio
-
-  $fileType = pathinfo($targetFilePath,PATHINFO_EXTENSION); // salvo formato file
-
-  move_uploaded_file($_FILES["file"]["tmp_name"], $targetFilePath); // salva il file caricato nella destinazione $targeFilePath
-
-  $watermarkImg = imagecreatefrompng($watermarkImagePath); // crea una nuova immagine dal path del watermark
-  switch($fileType){ 
-      case 'jpg': 
-        $dist = imagecreatefromjpeg($targetFilePath); 
-        break; 
-      case 'jpeg': 
-        $dist = imagecreatefromjpeg($targetFilePath); 
-        break; 
-      case 'png': 
-        $dist = imagecreatefrompng($targetFilePath); 
-        break; 
-      default: 
-        $dist = imagecreatefromjpeg($targetFilePath); 
-  } 
-  // crea una nuova immagine presa dal percorso di salvataggio
-
-  // aggiungo il filtro
-  if($_POST['filter'] == 0) {
-    imagefilter($dist, IMG_FILTER_GRAYSCALE);
+    } else {
+      $statusMsg = "<h4 class='alert alert-danger'>No files to upload!</h4>";
+    }
   }
-  if($_POST['filter'] == 1){
-    imagefilter($dist, IMG_FILTER_NEGATE);
-  }
 
-  // Set the margins for the watermark 
-  $marge_right = 50; 
-  $marge_bottom = 50; 
-    
-  // Get the height/width of the watermark image 
-  $lunghezza = imagesx($watermarkImg); 
-  $altezza = imagesy($watermarkImg); 
-    
-  // Copy the watermark image onto our photo using the margin offsets and  
-  // the photo width to calculate the positioning of the watermark. 
-  imagecopy($dist, $watermarkImg, (imagesx($dist) - $lunghezza - $marge_right), (imagesy($dist) - $altezza - $marge_bottom), 0, 0, imagesx($watermarkImg), imagesy($watermarkImg)); 
 
-  // Save image and free memory 
-  imagepng($dist, $targetFilePath); 
-  imagedestroy($dist);
+
+
+
+
+
+
+
 ?>
